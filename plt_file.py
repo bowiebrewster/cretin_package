8a4400f9042ad93ec21aac5c1404379526a36402
bowiebrewster <bowie.brewster@gmail.com>
2023-12-01 16:17:15 +0100

    pushed via gits.sh

--- plt_file.py
+++ plt_file.py
@@ -4,57 +4,36 @@ reload(paths)
 import numpy as np
 import h5py, os, glob, shutil
 import matplotlib.pyplot as plt
+import pandas as pd
+import io
+import seaborn as sns
 
-# We use this file to turn existing data structure 
+def parse_data(folder_path):
+    file_path = plt_path(folder_path)
 
+    with open(file_path, 'r') as file:
+        text = file.read()
+    sections = text.split('#')[1:]  # Split by sections, ignore first empty part
+    dataframes = {}
 
-# this is an absolute monstrosity of string manipulation
-def restructure(line : str, count : int):
-    if line[0] == '#':
-        name = line.split(' ')[1]
-        start_lines[name] = count
+    for section in sections:
+        lines = section.strip().split('\n')
+        title = lines[0].strip()
+        columns = lines[1].strip().split()[1:]  # Ignore the dollar sign
+        data = '\n'.join(lines[3:])
+        df = pd.read_csv(io.StringIO(data), delim_whitespace=True, names=columns)
+        dataframes[title] = df
 
-    elif count -1 in start_lines.values():
-        global plot_count, vars, data
-        plot_count +=1
-        vars = line.split('         ')
-
-        vars = [var.strip(' ').strip('$') for var in vars]
-        if vars[-1] == '\n':
-            vars = vars[0:-1]
-
-        if '\n' in vars[-1]:
-            vars[-1] = ' '.join(vars[-1].split('\n')[:-1]).strip(' ')
-
-        vars[0] = vars[0].strip(' ')
-        vars = [plot_count] + vars
-
-        vars = tuple(vars)
-        data[vars] = []
-
-    if line[0] not in ['$', '#'] and 'vars' in globals():
-        line = line.split(' ')
-        line[-1] =' '.join(line[-1].split('\n')[:-1])
-
-        newline = []
-        for entry in line:
-            if len(entry) >3:
-                newline.append(entry)
-
-        data[vars].append(newline)
+    return(dataframes)
 
 
-def create_plot(folder_name : str, path:str = paths.to_personal_data(), multiplot : bool = False):
+def create_plot(folder_name : str, path:str = paths.to_personal_data(), multiplot : bool = False, logplot : bool = False):
     folder = f'{path}{folder_name}'
     global data, start_lines, plot_count
     data, start_lines, plot_count = {},{},0
 
-    with open(plt_path(folder)) as f:
-        lines = f.readlines()
-
-        for count, line in enumerate(lines):
-            restructure(line, count)
-
+    data = parse_data(folder)
+    print(f'adding additional plot to {folder}/images')
     if multiplot:
         return data
 
@@ -62,28 +41,72 @@ def create_plot(folder_name : str, path:str = paths.to_personal_data(), multiplo
         os.makedirs(f'{folder}/images')
         print(f'Created image folder at {folder}/images')
 
+    # Define x variables
+    xvars = 'cycle,iter,time,ir,r,cdens,x2d,y2d,z2d,x3d,y3d,z3d,xy,k,kx,ky,kz,kr,l,lx,ly,lz,lr,m,mx,my,mz,mr,ifr,energy,freq,wvl,ebins,fbins,wbins,ifrline,evline,isp,sp_energy,sp_freq,sp_nu,sp_wvl,ie,iso,level,elev'
+    xvars_set = set(xvars.split(","))
+    for title, df in data.items():
 
-    for key, value in data.items():
-        print(key)
-        value = np.array(value)
-        value = value.astype('float')
+        # Check which columns in df are x variables
+        xvars_in_cols = [col for col in df.columns if col in xvars_set]
 
-        col_count = 0
-        for column in value.T:
-            if col_count == 0:
-                col0 = column
-            else:
-                plt.plot(col0, column)
-            col_count += 1
+        # Count the number of x variables in df
+        num_xvars = len(xvars_in_cols)
+        if num_xvars == 1:
+            plot1d(folder, title, df, xvars_set, xvars_in_cols, logplot)
 
-        plt.title(f'added plot {key}')
-        plt.xlabel(key[1])
-        plt.ylabel(key[2])
-        plt.savefig(f'{folder}/images/{key}.png')
-        plt.clf()
-        plt.close()
+        elif num_xvars == 2:
+            plot2d(folder, title, df, xvars_in_cols, logplot)
 
-    del data, start_lines, plot_count                    
+        else:
+            raise Exception('must be 2 or less x variables')
+
+
+def plot1d(folder, title, df, xvars_set, xvars_in_cols, logplot):
+    xvar = xvars_in_cols[0]
+    plt.figure()
+    y_labels = []        
+    for yvar in df.columns:
+        if yvar not in xvars_set:
+            plt.plot(df[xvar], df[yvar])
+            y_labels.append(yvar)
+
+    if logplot:
+        plt.yscale('log')
+
+    plt.xlabel(xvar)
+    plt.ylabel(y_labels)
+    plt.title(title)
+    plt.gca().invert_yaxis()
+    plt.savefig(f'{folder}/images/{title}.png')
+    plt.close()
+
+
+
+def plot2d(folder, title, df, xvars_in_cols, logplot):
+    if len(df.columns) != 3:
+        raise Exception('When using 2 xvariables to create a heatplot, there can only be one yvar since plots can not be overlayd')
+    # Plot heatmaps for each combination of x variables and y variables
+    xvar1, xvar2 = xvars_in_cols
+    yvar = set(df.columns) - set(xvars_in_cols)
+    g = f'{folder}/images/{title}_{yvar}_Heatmap'
+    
+    plt.figure()
+    heatmap_data = df.pivot_table(index=xvar1, columns=xvar2, values=yvar)
+    if logplot:
+        # Handling zeros or negative values in the data
+        heatmap_data = heatmap_data.replace(0, np.nan)
+        min_positive = heatmap_data.min().min()
+        heatmap_data = heatmap_data.fillna(min_positive)
+        heatmap_data = heatmap_data.clip(lower=min_positive)
+
+        # Applying a logarithmic transformation
+        heatmap_data = np.log(heatmap_data)
+    sns.heatmap(heatmap_data)
+    plt.xlabel(xvar2)
+    plt.ylabel(xvar1)
+    plt.title(title)
+    plt.savefig(g)
+    plt.close()
 
 
 def plt_path(folder : str):
